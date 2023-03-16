9b3b0588bd3c51a5fbaa329cb6af2d6cab7713c4
Rukshan <ruk.fernando93@gmail.com>
2023-03-17 00:45:58 +0100

    serverless export

--- workspaces/scripts/finalize-build.js
+++ workspaces/scripts/finalize-build.js
@@ -20,6 +20,8 @@ const exec = () => {
 
 const execServerless = () => {
   fs.copySync(getPath("client/dist"), getPath("server/dist"));
+  fs.removeSync(getPath("server/dist/api/index.js"));
+  fs.renameSync(getPath("server/dist/api/serverless.js"),getPath("server/dist/api/index.js"));
 };
 
 if (process.env.SERVERLESS) {
--- workspaces/server/src/index.ts
+++ workspaces/server/src/index.ts
@@ -1,29 +1,5 @@
-import express, { Request, Response } from "express";
-import path from "path";
-import proxy from "express-http-proxy";
 import app from "./app";
-
-const devMode = process.env.NODE_ENV === "dev";
-const clientPort = process.env.CLIENT_PORT ?? 5173;
-const port = process.env.PORT;
-
-app.get("/api", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+const port = process.env.PORT ?? 3009;
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
-
-if (devMode) {
-  app.use("/", proxy(`localhost:${clientPort}`));
-} else {
-  app.use(express.static(path.join(__dirname, "client"), { index: false }));
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/index.html"));
-  });
-}
-
-if (process.env.STANDALONE) {
-  app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-  });
-}
-
-export default app;
