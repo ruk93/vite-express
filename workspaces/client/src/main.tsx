d75870de6dfd550c24db470cf28c2e4a67a58d42
Rukshan <ruk.fernando93@gmail.com>
2023-03-18 01:57:07 +0100

    added react router dom

--- workspaces/client/src/main.tsx
+++ workspaces/client/src/main.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./features/landingPage/LandingPage";
+import NotFound from "./features/notFoundPage/NotFound";
 import "./index.css";
 import { store } from "./library/store";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <LandingPage />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<LandingPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
