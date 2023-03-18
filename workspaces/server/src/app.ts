d50f3fbed653dd754b6c435468064c0ce7e1966e
Rukshan <ruk.fernando93@gmail.com>
2023-03-18 02:53:13 +0100

    added compression

--- workspaces/server/src/app.ts
+++ workspaces/server/src/app.ts
@@ -1,11 +1,12 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import clientEnvironmentVariables from "./features/clientEnvironmentVariables";
+import compression from "compression";
 
 dotenv.config();
 
 const app: Express = express();
-
+app.use(compression());
 app.get("/api", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
